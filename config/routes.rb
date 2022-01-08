29221e550946d60570417cc3115d74c1902b46b5
heber <heberlazo@gmail.com>
2022-01-08 17:50:10 -0500

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,12 @@ Rails.application.routes.draw do
   resources :sessions, only: [:create]
   resources :users, only: [:create]
 
+  resources :conversations, only: [:index, :create]
+  resources :messages, only: [:create]
+
+  mount ActionCable.server => '/cable'
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
