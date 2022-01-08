3eeb75593e89d92db107967711483eefb5a3ad49
heber <heberlazo@gmail.com>
2022-01-08 17:48:15 -0500

    Added model conversation and message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_28_030853) do
+ActiveRecord::Schema.define(version: 2022_01_08_224804) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,20 @@ ActiveRecord::Schema.define(version: 2021_12_28_030853) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
     t.index ["user_id"], name: "index_books_on_user_id"
+  end
+
+  create_table "conversations", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "text"
+    t.integer "conversation_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,4 +43,5 @@ ActiveRecord::Schema.define(version: 2021_12_28_030853) do
   end
 
   add_foreign_key "books", "users"
+  add_foreign_key "messages", "conversations"
 end
