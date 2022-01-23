072880a5058fd4b6ecc405b979227ce03ff3d230
heber <heberlazo@gmail.com>
2022-01-23 10:46:48 -0500

    Added collumn to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_23_154353) do
+ActiveRecord::Schema.define(version: 2022_01_23_154600) do
 
   create_table "applications", force: :cascade do |t|
     t.boolean "status"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2022_01_23_154353) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "applications", "jobs"
